1a0391765f3bb9c78de5b9777bfe2776cefe4ae5
John Trupiano <jtrupiano@gmail.com>
2010-03-14 20:59:51 -0400

    Use factories for base_user_controller tests

--- test/functional/base_user_controller_test.rb
+++ test/functional/base_user_controller_test.rb
@@ -6,50 +6,60 @@ class BaseUserController < ApplicationController
   end
 end
 
+ActionController::Routing::Routes.draw do |map|
+  map.mock_action '/mock_action', :controller => 'base_user', :action => 'mock_action'
+end
+
 class BaseUserControllerTest < ActionController::TestCase
-  context "When accessing a public page with baltimore's subdomain" do
+  context "With Baltimore and DC Ignites" do
     setup do
-      set_host(baltimore)
-      get :mock_action
+      @baltimore = Factory(:ignite, :city => 'Baltimore', :domain => 'ignitebaltimore.com')
+      @dc        = Factory(:ignite, :city => 'DC', :domain => 'ignite-dc.com')
     end
     
-    should_respond_with :success
-    should "load baltimore" do
-      assert_equal baltimore.id, assigns(:ignite).id
-    end
-  end
-  
-  context "When accessing a public page with baltimore's subdomain prefixed with www" do
-    setup do
-      set_host("www.#{baltimore.domain}.com")
-      get :mock_action
-    end
+    context "When accessing a public page with baltimore's subdomain" do
+      setup do
+        set_host(@baltimore)
+        get :mock_action
+      end
     
-    should_respond_with :success
-    should "load baltimore" do
-      assert_equal baltimore.id, assigns(:ignite).id
-    end    
-  end
-  
-  context "When accessing a public page with dc's subdomain" do
-    setup do
-      set_host(dc)
-      get :mock_action
+      should_respond_with :success
+      should "load baltimore" do
+        assert_equal @baltimore.id, assigns(:ignite).id
+      end
     end
+  
+    context "When accessing a public page with baltimore's subdomain prefixed with www" do
+      setup do
+        set_host("www.#{@baltimore.domain}.com")
+        get :mock_action
+      end
     
-    should_respond_with :success
-    should "load baltimore" do
-      assert_equal dc.id, assigns(:ignite).id
+      should_respond_with :success
+      should "load baltimore" do
+        assert_equal @baltimore.id, assigns(:ignite).id
+      end    
     end
-  end
   
-  context "When accessing a public site with an invalid subdomain" do
-    setup do
-      @request.host = 'somethingwedonthave.com'
-      get :mock_action
-    end
+    context "When accessing a public page with dc's subdomain" do
+      setup do
+        set_host(@dc)
+        get :mock_action
+      end
     
-    should_respond_with 404
-  end
+      should_respond_with :success
+      should "load baltimore" do
+        assert_equal @dc.id, assigns(:ignite).id
+      end
+    end
   
+    context "When accessing a public site with an invalid subdomain" do
+      setup do
+        @request.host = 'somethingwedonthave.com'
+        get :mock_action
+      end
+    
+      should_respond_with 404
+    end
+  end
 end
