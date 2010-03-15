e8c3a607965f5d5f1b3bd4ea85f5d63928528ae9
John Trupiano <jtrupiano@gmail.com>
2010-03-14 20:48:31 -0400

    Use factories for sessions_controller_test

--- test/functional/sessions_controller_test.rb
+++ test/functional/sessions_controller_test.rb
@@ -1,11 +1,16 @@
 require File.dirname(__FILE__) + '/../test_helper'
-require 'sessions_controller'
-
-# Re-raise errors caught by the controller.
-class SessionsController; def rescue_action(e) raise e end; end
 
 class SessionsControllerTest < ActionController::TestCase
 
+  def setup
+    @admin = Factory(:admin, :login => 'ggentzke', :password => 'ggentzke', :password_confirmation => 'ggentzke')
+    set_host @admin.ignite
+  end
+  
+  def teardown
+    log_out
+  end
+  
   def test_should_login_and_redirect
     post :create, :login => 'ggentzke', :password => 'ggentzke'
     assert session[:admin_id]
@@ -19,9 +24,8 @@ class SessionsControllerTest < ActionController::TestCase
   end
 
   def test_should_logout
-    log_in ggentzke do
-      get :destroy
-    end
+    log_in @admin
+    get :destroy
     assert_nil session[:admin_id]
     assert_response :redirect
   end
@@ -29,14 +33,12 @@ class SessionsControllerTest < ActionController::TestCase
   def test_should_not_remember_me
     @request.cookies["auth_token"] = nil
     post :create, :login => 'ggentzke', :password => 'ggentzke'
-    puts @response.cookies["auth_token"]
     assert @response.cookies["auth_token"].blank?
   end
   
   def test_should_delete_token_on_logout
-    log_in ggentzke do
-      get :destroy
-    end
+    log_in @admin
+    get :destroy
     assert @response.cookies["auth_token"].blank?
   end
 
