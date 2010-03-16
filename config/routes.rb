c88f41d9c5dd3a4ff276423d25d68dfdbd465e00
John Trupiano <jtrupiano@gmail.com>
2010-03-16 13:40:58 -0400

    Allow flash messages to span more than a single line in a pleasing manner. Use the proper URL for viewing all proposals from the speaker page

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ ActionController::Routing::Routes.draw do |map|
   map.resources :organizers, :only => [:index, :show]
   map.resources :speakers, :only => [:index, :show], 
     :collection => {:post_comment => :post, :proposals => :get}
-  map.resources :proposals, :only => [:new, :create], :collection => {:post_comment => :post}
+  map.resources :proposals, :only => [:new, :create]
 
   map.namespace(:admin) do |admin|
     admin.resources :admins
