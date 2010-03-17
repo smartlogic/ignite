bb502f6ef489f4d7c8b4900301b22f2250198d1e
John Trupiano <jtrupiano@gmail.com>
2010-03-16 23:28:37 -0400

    Completely remove dependency on sanitize

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,19 +1,4 @@
 class Comment < ActiveRecord::Base
   belongs_to :parent, :polymorphic => true
-  
   validates_presence_of :author, :content
-  
-  before_save :clean_attrs
-  
-  private
-  
-  def clean_attrs
-    %w(content author email).each do |attr|
-      self.send("#{attr}=", ContentCleaner.clean(self.send(attr))) unless self.send(attr).nil?
-    end
-    %w(url).each do |attr|
-      self.send("#{attr}=", ContentCleaner.fix_link(self.send(attr))) unless self.send(attr).nil?
-    end
-  end
-  
 end
