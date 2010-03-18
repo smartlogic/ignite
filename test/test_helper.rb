69602d2c3ddd57b9eae644df3808c7d71fe8ba62
John Trupiano <jtrupiano@gmail.com>
2010-03-18 19:47:28 -0400

    Add leftright gem for improved test running output

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,7 +1,7 @@
 ENV["RAILS_ENV"] = "test"
 require File.expand_path(File.dirname(__FILE__) + "/../config/environment")
 require 'test_help'
-require 'will_paginate'
+require 'leftright'
 
 require 'shoulda'
 require 'factory_girl'
