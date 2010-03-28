25d3469ff346413d897577b6c628f701eb4eaff4
John Trupiano <jtrupiano@gmail.com>
2010-03-28 19:28:19 -0400

    Use full paths for links in emails

--- config/development/environment.rb
+++ config/development/environment.rb
@@ -15,6 +15,7 @@ config.action_controller.perform_caching             = false
 
 # Don't care if the mailer can't send
 config.action_mailer.raise_delivery_errors = false
+# config.action_mailer.delivery_method = :smtp
 
 # Keys for reCaptcha
 RCC_PUB = '6LdWEwMAAAAAAEBGCE8ddKD6odUtz84AECYVPAPd'
