cf39eeb838eb64b1816518f7c9df98e91665a2eb
John Trupiano <jtrupiano@gmail.com>
2010-03-28 21:35:08 -0400

    Send HTML emails, fix a typo

--- app/models/proposal_notifier.rb
+++ app/models/proposal_notifier.rb
@@ -2,14 +2,14 @@ class ProposalNotifier < ActionMailer::Base
   
   def admin_notification(proposal)
     recipients  proposal.ignite.emails_as_array
-    from        "#{proposal.ignite.name} /<no-reply@#{proposal.ignite.domain}/>"
+    from        "#{proposal.ignite.name} \<no-reply@#{proposal.ignite.domain}\>"
     subject     "#{proposal.name} submitted proposal #{proposal.title}"
     body        ({ :proposal => proposal })
   end
   
   def thank_you(proposal)
     recipients  proposal.email
-    from        "#{proposal.ignite.name} /<no-reply@#{proposal.ignite.domain}/>"
+    from        "#{proposal.ignite.name} \<no-reply@#{proposal.ignite.domain}\>"
     subject     "Thank you for submitting your proposal"
     body        ({ :proposal => proposal })
   end
