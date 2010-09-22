7716f29ad6f41ec8a54cbffa3398e6e0423aafac
John Trupiano <jtrupiano@gmail.com>
2010-09-22 19:20:02 -0400

    whoops

--- config/initializers/rack-rewrite.rb
+++ config/initializers/rack-rewrite.rb
@@ -1,7 +1,7 @@
 require 'rack-rewrite'
 ActionController::Dispatcher.middleware.insert_before(Rack::Lock, Rack::Rewrite) do
   # Redirect all requests to live.ignitebaltimore.com to the live stream
-  r302 /.*/, "http://ignitebaltimore.com/articles/76-Live", :if => { |rack_env|
+  r302 /.*/, "http://ignitebaltimore.com/articles/76-Live", :if => Proc.new { |rack_env|
     rack_env['SERVER_NAME'] == 'live.ignitebaltimore.com'
   }
   r301 "/proposals/", "/speakers/proposals"
